598278cd1aab51906fa6bfc066e84d426864b1b3
user <user@usertekimac-mini.local>
2016-03-16 13:43:53 +0800

    Initial Commit

---
+++ Object/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Object
+//
+//  Created by user on 16/3/16.
+//  Copyright © 2016年 GuoBIn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Object/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Object
+//
+//  Created by user on 16/3/16.
+//  Copyright © 2016年 GuoBIn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
